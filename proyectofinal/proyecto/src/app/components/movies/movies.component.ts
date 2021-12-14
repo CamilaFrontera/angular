e6449786dfe6c5e94f38d483e85a3907cda8b89a
cfrontera <camilasofiafront@gmail.com>
2021-12-14 14:06:02 -0300

    cambios

--- proyectofinal/proyecto/src/app/components/movies/movies.component.ts
+++ proyectofinal/proyecto/src/app/components/movies/movies.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
+
+  movies = [
+    {
+    nombre: 'La sirenita',
+    calificacion: 'niños',
+    imagen: 'https://m.media-amazon.com/images/I/711Bjf+3KuL._AC_SY679_.jpg'
+  },
+  {
+    nombre: 'Cenicienta',
+    calificacion: 'niños',
+    imagen: 'https://http2.mlstatic.com/D_NQ_NP_937656-MLA25958929008_092017-O.webp'
+  },
+  {
+    nombre: 'Blanca nieves',
+    calificacion: 'niños',
+    imagen: 'https://http2.mlstatic.com/D_NQ_NP_765866-MLA25759897064_072017-O.webp'
+  },
+  {
+    nombre: 'Lilo y stitch',
+    calificacion: 'niños',
+    imagen: 'https://m.media-amazon.com/images/I/71DhCXgo5xL._AC_SY606_.jpg'
+  }
+]
   constructor() { }
 
   ngOnInit(): void {
--- proyectofinal/proyecto/src/app/components/register/register.component.ts
+++ proyectofinal/proyecto/src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -16,7 +17,7 @@ export class RegisterComponent implements OnInit {
     nombre: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(12)]),
     apellido: new FormControl('', [Validators.required, Validators.email, Validators.minLength(3), Validators.maxLength(12)]),
     email: new FormControl('', [Validators.required, Validators.email,  Validators.maxLength(35)]),
-    telefono: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)] ),
+    // telefono: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)] ),
     fechaNacimiento: new FormControl('', Validators.required),
     contraseña: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(9)])
   });
@@ -25,13 +26,56 @@ export class RegisterComponent implements OnInit {
   nombreControl = this.registerForm.controls['nombre'];
   apellidoControl = this.registerForm.controls['apellido'];
   emailControl = this.registerForm.controls['email'];
-  telefonoControl = this.registerForm.controls['telefono'];
+  // telefonoControl = this.registerForm.controls['telefono'];
   fechaNacimientoControl = this.registerForm.controls['fechaNacimiento'];
   passwordControl = this.registerForm.controls['contraseña'];
 
   ngOnInit(): void {
   }
 
+  accounts = [
+    {
+      name: 'Camila',
+      lastname: 'Frontera',
+      email: 'cfrontera@gmail.com',
+      fechaNacimiento: '06/07/1997',
+      password: '1234'
+    },
+    {
+      name: 'Juan',
+      lastname: 'Pérez',
+      email: 'jperez@gmail.com',
+      fechaNacimiento: '04/12/1991',
+      password: '123445'
+    },
+    {
+      name: 'Pedro',
+      lastname: 'Montiel',
+      email: 'pedroMont@gmail.com',
+      fechaNacimiento: '12/01/2001',
+      password: '12345'
+    },
+    {
+      name: 'Ana',
+      lastname: 'Benavidez',
+      email: 'ab_aguantebokita@gmail.com',
+      fechaNacimiento: '13/11/1979',
+      password: '1234dfgdfg'
+    },
+
+  ]
+
+  addAccount(name: string, lastname: string, email: string, fechaNacimiento: string, password: string ){
+    this.accounts.push({
+      name: name,
+      lastname: lastname,
+      email: email,
+      fechaNacimiento: fechaNacimiento,
+      password: password,
+
+    })
+
+  }
   save(){
 
   if (this.registerForm.valid){
