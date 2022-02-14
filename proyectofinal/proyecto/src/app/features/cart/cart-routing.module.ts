da5d4573fec874d1f9e75a8cfdc1bb9e64fb846f
cfrontera <camilasofiafront@gmail.com>
2022-02-14 19:41:17 -0300

    features

--- proyectofinal/proyecto/src/app/features/cart/cart-routing.module.ts
+++ proyectofinal/proyecto/src/app/features/cart/cart-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartComponent } from './components/cart.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: CartComponent
+  }
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class CartRoutingModule { }
--- proyectofinal/proyecto/src/app/features/cart/cart.module.ts
+++ proyectofinal/proyecto/src/app/features/cart/cart.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './components/cart.component';
+import { CartRoutingModule } from './cart-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { cartReducer } from './components/store/cart.reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { CartEffects } from './components/store/cart.effects';
+import { CartService } from './services/cart.service';
 
 
 
@@ -9,7 +15,13 @@ import { CartComponent } from './components/cart.component';
     CartComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CartRoutingModule,
+    StoreModule.forFeature('cart', cartReducer),
+    EffectsModule.forFeature([CartEffects])
+  ],
+  providers: [
+    CartService
   ]
 })
 export class CartModule { }
